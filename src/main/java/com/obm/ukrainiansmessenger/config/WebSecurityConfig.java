97ddea63a4dbe3bc595851ef6b9cc1397411bc60
Aleksander <zagorenkosahsa@gmail.com>
2022-07-14 22:17:24 +0300

    Add registration with email

--- src/main/java/com/obm/ukrainiansmessenger/config/WebSecurityConfig.java
+++ src/main/java/com/obm/ukrainiansmessenger/config/WebSecurityConfig.java
@@ -1,17 +1,11 @@
 package com.obm.ukrainiansmessenger.config;
 
 import com.obm.ukrainiansmessenger.servise.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.
                 authorizeRequests()
-                .antMatchers("/","/registration").permitAll()
+                .antMatchers("/","/registration","/activate/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/com/obm/ukrainiansmessenger/servise/UserService.java
+++ src/main/java/com/obm/ukrainiansmessenger/servise/UserService.java
@@ -1,5 +1,6 @@
 package com.obm.ukrainiansmessenger.servise;
 
+import com.obm.ukrainiansmessenger.models.Role;
 import com.obm.ukrainiansmessenger.models.User;
 import com.obm.ukrainiansmessenger.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,21 +8,60 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final MailSender mailSender;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, MailSender mailSender) {
         this.userRepository = userRepository;
+        this.mailSender = mailSender;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+        if (userFromDb != null){
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepository.save(user);
+        if(user.getEmail()!=null){
+            String message = String.format(
+                    "Привіт, %s\n" +
+                            "Ласкаво просимо до Українського Месенджера. Будь ласка, перейдіть за посиланням: http://localhost:8080/active/%s для підтвердження вашої пошти.",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSender.send(user.getEmail(), "Код активації", message);
+        }
+
+        return true;
+    }
+
+    public MailSender getMailSender() {
+        return mailSender;
+    }
+
+    public boolean activeUser(String code) {
+        User user = userRepository.findByActivationCode(code);
+        if(user==null){
+            return false;
+        }
+        user.setActivationCode(null);
+        userRepository.save(user);
+
+        return true;
     }
 }
