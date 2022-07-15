81fc472f604c816fcf6fd70eaaafb0e3ab67eab4
Дмитрий <dimabukov52@gmail.com>
2022-07-15 17:57:34 +0300

    webscoket 0.0.3(add chatRoom and message with creation tables in DB)

--- src/main/java/com/obm/ukrainiansmessenger/controller/GreetingController.java
+++ src/main/java/com/obm/ukrainiansmessenger/controller/GreetingController.java
@@ -4,7 +4,6 @@ import com.obm.ukrainiansmessenger.models.Greeting;
 import com.obm.ukrainiansmessenger.models.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
