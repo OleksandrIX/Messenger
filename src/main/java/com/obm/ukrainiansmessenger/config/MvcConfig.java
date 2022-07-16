f80e9388d5ecd978cd0064cc34f08ae56f2d705f
Aleksander <zagorenkosahsa@gmail.com>
2022-07-16 12:22:10 +0300

    add registration through email 1.1

--- src/main/java/com/obm/ukrainiansmessenger/config/MvcConfig.java
+++ src/main/java/com/obm/ukrainiansmessenger/config/MvcConfig.java
@@ -11,6 +11,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/chat").setViewName("chat");
         registry.addViewController("/").setViewName("login");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/index").setViewName("index");
         registry.addViewController("/registration").setViewName("registration");
     }
 }
